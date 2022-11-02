2204e01cafee2c2089183c4c5fb9e6b29dfba0ba
Tomáš Jaroš <xjaros2@fi.muni.cz>
2022-11-02 13:53:21 +0100

    allow for conditional parsing of just chosen type of profile

--- AlgTest_pyProcess/algtestprocess/modules/parser/getters.py
+++ AlgTest_pyProcess/algtestprocess/modules/parser/getters.py
@@ -4,13 +4,21 @@ import sys
 
 from math import inf
 
-from algtestprocess.modules.jcalgtest import ProfilePerformanceFixedJC, \
-    ProfilePerformanceVariableJC
-from algtestprocess.modules.parser.javacard.performance import \
-    get_files_to_process, PerformanceParserJC, \
-    create_sorted_already_measured_list, fix_error_codes, \
-    fix_missing_underscores, fix_missing_variable_data_lengths, convert_to_json, \
-    prepare_missing_measurements, compute_stats
+from algtestprocess.modules.jcalgtest import (
+    ProfilePerformanceFixedJC,
+    ProfilePerformanceVariableJC,
+)
+from algtestprocess.modules.parser.javacard.performance import (
+    get_files_to_process,
+    PerformanceParserJC,
+    create_sorted_already_measured_list,
+    fix_error_codes,
+    fix_missing_underscores,
+    fix_missing_variable_data_lengths,
+    convert_to_json,
+    prepare_missing_measurements,
+    compute_stats,
+)
 from algtestprocess.modules.parser.javacard.support import SupportParserJC
 from algtestprocess.modules.parser.tpm.cryptoprops import CryptoProps
 from algtestprocess.modules.parser.tpm.performance import PerformanceParserTPM
@@ -19,24 +27,25 @@ from algtestprocess.modules.parser.tpm.support import SupportParserTPM
 
 def get_javacard_profiles(directory, preprocess: bool):
     """
-        When parsing profiles for JavaCards, it is assumed that fixed and variable
-        results were processed before and *.json outputs were created. That means
-        script was called with `process` argument.
+    When parsing profiles for JavaCards, it is assumed that fixed and variable
+    results were processed before and *.json outputs were created. That means
+    script was called with `process` argument.
 
-        Following path illustrate valid result directories
+    Following path illustrate valid result directories
 
-        Performance results folder with *.json files
-        {directory}/.*/[Pp]erformance/(fixed|variable)/
+    Performance results folder with *.json files
+    {directory}/.*/[Pp]erformance/(fixed|variable)/
 
-        Support results folder with *.csv files
-        {directory}/.*/[rR]esults/
+    Support results folder with *.csv files
+    {directory}/.*/[rR]esults/
 
     """
 
     perf_dir = [
         os.path.join(root, dirname)
         for root, dirnames, _ in os.walk(directory)
-        for dirname in dirnames if dirname.lower() == 'performance'
+        for dirname in dirnames
+        if dirname.lower() == "performance"
     ]
 
     if preprocess:
@@ -47,34 +56,42 @@ def get_javacard_profiles(directory, preprocess: bool):
     files_performance = get_files_to_process(directory, ".json")
 
     if not files_performance:
-        print("get_javacard_profiles:"
-              " script needs to be called with process argument first")
+        print(
+            "get_javacard_profiles:"
+            " script needs to be called with process argument first"
+        )
         sys.exit(1)
 
     files_support = get_files_to_process(directory, ".csv")
 
-    profiles_fixed = list(map(
-        lambda x: PerformanceParserJC(x).parse(ProfilePerformanceFixedJC()),
-        filter(
-            lambda y: "/performance/" in y.lower() and "/fixed/" in y,
-            files_performance
-        )))
+    profiles_fixed = list(
+        map(
+            lambda x: PerformanceParserJC(x).parse(ProfilePerformanceFixedJC()),
+            filter(
+                lambda y: "/performance/" in y.lower() and "/fixed/" in y,
+                files_performance,
+            ),
+        )
+    )
     rename_duplicates(profiles_fixed)
-    profiles_variable = list(map(
-        lambda x: PerformanceParserJC(x).parse(ProfilePerformanceVariableJC()),
-        filter(
-            lambda y: "/performance/" in y.lower() and "/variable/" in y,
-            files_performance
-        )))
+    profiles_variable = list(
+        map(
+            lambda x: PerformanceParserJC(x).parse(ProfilePerformanceVariableJC()),
+            filter(
+                lambda y: "/performance/" in y.lower() and "/variable/" in y,
+                files_performance,
+            ),
+        )
+    )
     rename_duplicates(profiles_variable)
     profiles_variable = sorted(profiles_variable, key=lambda x: x.device_name().lower())
 
-    profiles_support = list(map(
-        lambda x: SupportParserJC(x).parse(),
-        filter(
-            lambda y: "/results/" in y.lower(),
-            files_support
-        )))
+    profiles_support = list(
+        map(
+            lambda x: SupportParserJC(x).parse(),
+            filter(lambda y: "/results/" in y.lower(), files_support),
+        )
+    )
     rename_duplicates(profiles_support)
     profiles_support = sorted(profiles_support, key=lambda x: x.device_name().lower())
 
@@ -88,90 +105,103 @@ def tpm_sorted(profiles, device_name):
 
     Assumes device name is in the form of rgx
     """
-    RGX = r'(\s*.+)+\s\s*\d+(\.\d+)*(\s[\[]\d+[\]])?'
+    RGX = r"(\s*.+)+\s\s*\d+(\.\d+)*(\s[\[]\d+[\]])?"
     try:
-        assert all(
-            [re.match(RGX, device_name(p)) is not None for p in profiles]
-        )
+        assert all([re.match(RGX, device_name(p)) is not None for p in profiles])
     except AssertionError:
         print("These device name does not match format")
-        print(
-            [name
-             for p in profiles
-             if not re.match(RGX, (name := device_name(p)))]
-        )
+        print([name for p in profiles if not re.match(RGX, (name := device_name(p)))])
 
     def key_f(profile):
         manufacturer = version = idx = inf
         numbers = [inf] * 4
         l, r = device_name(profile).rsplit(maxsplit=1)
 
-        if re.match(r'[\[]\d+[\]]', r):
-            idx = int(r.replace('[', '').replace(']', ''))
+        if re.match(r"[\[]\d+[\]]", r):
+            idx = int(r.replace("[", "").replace("]", ""))
             manufacturer, firmware = l.rsplit(maxsplit=1)
         else:
             manufacturer, firmware = l, r
 
-        numbers = [int(x) for x in filter(None, firmware.split('.'))]
+        numbers = [int(x) for x in filter(None, firmware.split("."))]
 
         return [manufacturer] + numbers + [idx]
 
     return sorted(profiles, key=key_f)
 
 
-def get_tpm_profiles(directory, legacy):
+def get_tpm_profiles(
+    directory,
+    legacy,
+    parse_performance=True,
+    parse_support=True,
+    parse_cryptoprops=True,
+):
     """
-       When parsing tpm profiles
+    When parsing tpm profiles
 
-       IMPORTANT: Legacy profiles <= 10/2022
-       For performance profiles folder with *.csv results
-       {directory}/.*/[Pp]erformance/
+    IMPORTANT: Legacy profiles <= 10/2022
+    For performance profiles folder with *.csv results
+    {directory}/.*/[Pp]erformance/
 
-       For support profiles folder with *.csv results
-       {directory}/.*/[Rr]esults/
+    For support profiles folder with *.csv results
+    {directory}/.*/[Rr]esults/
 
-       For cryptographic properties folder with *.csv results
-       {directory}/.*/[Dd]etail/
+    For cryptographic properties folder with *.csv results
+    {directory}/.*/[Dd]etail/
     """
     files = [
         os.path.join(root, file)
-        for root, _, files in os.walk(directory) for file in files
+        for root, _, files in os.walk(directory)
+        for file in files
     ]
 
-    perf_parser = lambda path: PerformanceParserTPM(path).parse()
-    perf_name = lambda name: "performance.txt" in name.lower()
-    if legacy:
-        perf_parser = lambda path: PerformanceParserTPM(path).parse_legacy()
-        perf_name = lambda name: "/performance/" in name.lower() and ".csv" in name
+    performance = support = cryptoprops = []
 
-    performance = list(filter(perf_name, files))
-    performance = list(filter(
-        lambda profile: profile.results, map(perf_parser, performance)
-    ))
-    rename_duplicates(performance)
-    performance = tpm_sorted(performance, lambda x: x.device_name())
+    if parse_performance:
+        perf_parser = lambda path: PerformanceParserTPM(path).parse()
+        perf_name = lambda name: "performance.txt" in name.lower()
+        if legacy:
+            perf_parser = lambda path: PerformanceParserTPM(path).parse_legacy()
+            perf_name = lambda name: "/performance/" in name.lower() and ".csv" in name
 
-    support_parser = lambda path: SupportParserTPM(path).parse()
-    support_name = lambda name: "results.txt" in name
-    if legacy:
-        support_parser = lambda path: SupportParserTPM(path).parse_legacy()
-        support_name = lambda name: "/results/" in name.lower() and ".csv" in name
+        performance = list(filter(perf_name, files))
+        performance = list(
+            filter(lambda profile: profile.results, map(perf_parser, performance))
+        )
+        rename_duplicates(performance)
+        performance = tpm_sorted(performance, lambda x: x.device_name())
 
-    support = list(filter(support_name, files))
-    support = list(filter(
-        lambda profile: profile.results, map(support_parser, support)
-    ))
-    rename_duplicates(support)
-    support = tpm_sorted(support, lambda x: x.device_name())
+    if parse_support:
+        support_parser = lambda path: SupportParserTPM(path).parse()
+        support_name = lambda name: "results.txt" in name
+        if legacy:
+            support_parser = lambda path: SupportParserTPM(path).parse_legacy()
+            support_name = lambda name: "/results/" in name.lower() and ".csv" in name
 
-    cryptoprops_paths = list(set(
-        map(lambda match: match.group(1),
-            filter(None, map(lambda name: re.search(r'(.*/[Dd]etail/).*', name),
-                             files)))
-    ))
-    cryptoprops = list(filter(None, map(lambda path: CryptoProps(path).parse(),
-                                        cryptoprops_paths)))
-    tpm_sorted(cryptoprops, lambda x: x['device_name'])
+        support = list(filter(support_name, files))
+        support = list(
+            filter(lambda profile: profile.results, map(support_parser, support))
+        )
+        rename_duplicates(support)
+        support = tpm_sorted(support, lambda x: x.device_name())
+
+    if parse_cryptoprops:
+        cryptoprops_paths = list(
+            set(
+                map(
+                    lambda match: match.group(1),
+                    filter(
+                        None,
+                        map(lambda name: re.search(r"(.*/[Dd]etail/).*", name), files),
+                    ),
+                )
+            )
+        )
+        cryptoprops = list(
+            filter(None, map(lambda path: CryptoProps(path).parse(), cryptoprops_paths))
+        )
+        tpm_sorted(cryptoprops, lambda x: x["device_name"])
 
     return performance, support, cryptoprops
 
@@ -179,10 +209,9 @@ def get_tpm_profiles(directory, legacy):
 def rename_duplicates(profiles):
     """Renames the profiles with same name"""
     for profile in profiles:
-        duplicates = list(filter(
-            lambda x: x.device_name() == profile.device_name(),
-            profiles
-        ))
+        duplicates = list(
+            filter(lambda x: x.device_name() == profile.device_name(), profiles)
+        )
         if len(duplicates) > 1:
             for i, dupe in enumerate(duplicates):
                 dupe.rename(f"{dupe.device_name()} [{i + 1}]")
