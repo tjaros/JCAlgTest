51f00e9c408481a576f4aa7b43cfddba4295c408
Tomáš Jaroš <xjaros2@fi.muni.cz>
2022-11-03 16:48:25 +0100

    make images in the modal bigger

--- AlgTest_pyProcess/algtestprocess/modules/components/modal.py
+++ AlgTest_pyProcess/algtestprocess/modules/components/modal.py
@@ -4,17 +4,16 @@ from dominate import tags
 def modal():
     """Component which creates image preview"""
     with tags.div(
-            className="modal fade",
-            id="imagemodal",
-            tabindex="-1",
-            aria_labelledby="myModalLabel",
-            aria_hidden="true"
+        className="modal fade flex flex-column justify-content-center align-items-center",
+        id="imagemodal",
+        tabindex="-1",
+        aria_labelledby="myModalLabel",
+        aria_hidden="true",
     ):
-        with tags.div(className="modal-dialog"):
+        with tags.div(className="modal-dialog", style="max-width: 75%;"):
             with tags.div(className="modal-content"):
                 with tags.div(className="modal-body"):
-                    tags.img(src="", className="imagepreview",
-                             style="width: 100%;")
+                    tags.img(src="", className="imagepreview", style="width: 100%;")
 
 
 def modal_script():
